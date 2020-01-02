f41ec98578d90e6c65808ecfe1d7a5c5e6be1fc8
Jason Gorelick <jason@avenirhq.com>
2020-01-02 11:30:54 -0800

    adds branch protection feature

--- app.rb
+++ app.rb
@@ -6,53 +6,65 @@ require 'octokit'
 
 config_file 'config.yml'
 
+
 post '/payload' do
   request.body.rewind
   payload_body = request.body.read
   verify_signature(payload_body)
 
-  client = Octokit::Client.new(:access_token => settings.github_auth_token)
-
-  # =================================
-  # 
-  # These are the options noted in the API docs that are required for the PUT request 
-  # for branch protection.
-  #
-  # If we need to change these settings frequently, move to a config file.
-  # 
-  # =================================
-
-  protection_options = {
-    :required_status_checks => nil,
-    :enforce_admins => nil,
-    :required_pull_request_reviews => nil,
-    :restrictions => nil,
-    :required_linear_history => false,
-    :allow_force_pushes => false,
-    :allow_deletions => false,
-  }
-
-  if request.env["HTTP_X_GITHUB_EVENT"] == "repository"
+  if request.env["HTTP_X_GITHUB_EVENT"] == "create"
   # Only do something when receiving a payload for a repository
 
     push = JSON.parse(payload_body)
     org = push.dig("organization", "login")
-    action = push.dig("action")
+    ref = push.dig("ref")
+    ref_type = push.dig("ref_type")
+    master_branch = push.dig("master_branch")
     repo_id = push.dig("repository", "id")
 
-    if org == settings.github_org && action == "created"
-    # if we have created a new repo in our Org
+    # =================================
+    # 
+    # These are the options noted in the API docs that are required for the PUT request 
+    # for branch protection.
+    #
+    # If we need to change these settings frequently, move to a config file.
+    # 
+    # =================================
 
-      # @client.edit_branch_protection(repo_id, 'master', *options)
+    protection_options = {
+      :required_status_checks => nil,
+      :enforce_admins => nil,
+      :required_pull_request_reviews => nil,
+      :restrictions => nil,
+      :required_linear_history => false,
+      :allow_force_pushes => false,
+      :allow_deletions => false,
+    }
 
-      client.create_issue(
-          repo_id,
-          '[bot] Branch Protections Added to master branch',
-          "@#{settings.github_user},
-          \n\nBranch protections were added to this repository with options:
-          \n\n`#{protection_options.inspect}`")
+    if org == settings.github_org && ref_type == "branch" && ref == master_branch
+      # if this event is for the creation of a new master branch for this repo
+      add_branch_protection(repo_id, ref, protection_options)
+      create_gh_issue(repo_id, ref, protection_options)
     end
+
   end
+end
+
+private
+
+def add_branch_protection(repo_id, branch_name, options = {})
+  client = Octokit::Client.new(:access_token => settings.github_auth_token)
+  client.protect_branch(repo_id, branch_name, options.merge(accept: 'application/vnd.github.luke-cage-preview+json'))
+end
+
+def create_gh_issue(repo_id, branch_name, options)
+  client = Octokit::Client.new(:access_token => settings.github_auth_token)
+  client.create_issue(
+      repo_id,
+      "[bot] Branch Protections Added to #{branch_name} branch",
+      "@#{settings.github_user},
+      \n\nBranch protections were added to this repository with options:
+      \n\n```\n\n" + JSON.pretty_generate(options, {object_nl: "\n"}) + "\n\n```")
 end
 
 def verify_signature(payload_body)
